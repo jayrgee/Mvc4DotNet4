12db7a886ee7f29dacff53e8f78468558407b1ad
jayrgee <jayrgee@fastmail.fm>
2019-11-27 00:51:30 +1100

    add Guitars area

---
+++ Mvc4DotNet4/Areas/Guitars/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc4DotNet4.Areas.Guitars.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //
+        // GET: /Guitars/Default/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Mvc4DotNet4/Areas/Guitars/GuitarsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Mvc4DotNet4.Areas.Guitars
+{
+    public class GuitarsAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Guitars";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Guitars_default",
+                "Guitars/{controller}/{action}/{id}",
+                new { controller = "Default", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
