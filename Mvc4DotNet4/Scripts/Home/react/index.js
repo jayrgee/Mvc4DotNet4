dfd64fd4c281de1cb1ae87fa2a57d8f7e0ab4a04
jayrgee <jayrgee@fastmail.fm>
2019-12-01 02:41:29 +1100

    add react

---
+++ Mvc4DotNet4/Scripts/Home/react/index.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+    <React.Fragment>
+        <h1>React in ASP.NET MVC!</h1>
+        <h2>Hello foo World</h2>
+    </React.Fragment>
+);
+
+render(<App />, document.getElementById('my-react-app'));
