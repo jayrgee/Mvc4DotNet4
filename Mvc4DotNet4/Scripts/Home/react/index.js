2893562a5df8de631654cd3d09628e601f4271f2
jayrgee <jayrgee@fastmail.fm>
2019-12-01 12:38:58 +1100

    fix indentation, eol on JS files

--- Mvc4DotNet4/Scripts/Home/react/index.js
+++ Mvc4DotNet4/Scripts/Home/react/index.js
@@ -4,10 +4,10 @@ import { render } from 'react-dom';
 import Heading from './components/Heading';
 import Welcome from './components/Welcome';
 const App = () => (
-    <React.Fragment>
-        <Heading heading='React in ASP.NET MVC!' />
-        <Welcome name="foo" />
-    </React.Fragment>
+  <React.Fragment>
+    <Heading heading='React in ASP.NET MVC!' />
+    <Welcome name="foo" />
+  </React.Fragment>
 );
 
 render(<App />, document.getElementById('my-react-app'));
--- Mvc4DotNet4/webpack.config.js
+++ Mvc4DotNet4/webpack.config.js
@@ -5,22 +5,22 @@ var WebpackNotifierPlugin = require("webpack-notifier");
 var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
-    entry: "./Scripts/Home/react/index.js",
-    output: {
-        path: path.resolve(__dirname, "./Scripts/dist/Home/react"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    },
-    devtool: "inline-source-map",
-    plugins: [new WebpackNotifierPlugin(), new BrowserSyncPlugin()]
+  entry: "./Scripts/Home/react/index.js",
+  output: {
+    path: path.resolve(__dirname, "./Scripts/dist/Home/react"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  devtool: "inline-source-map",
+  plugins: [new WebpackNotifierPlugin(), new BrowserSyncPlugin()]
 };
